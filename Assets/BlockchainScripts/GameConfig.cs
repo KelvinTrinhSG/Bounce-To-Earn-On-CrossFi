f1cb15d413164864180e40d9f39a4d9cc394aa49
kelvintrinhsg <thkien85@Gmail.com>
2025-07-29 05:49:05 +0700

    Initial commit

---
+++ Assets/BlockchainScripts/GameConfig.cs
@@ -0,0 +1,12 @@
+﻿public static class GameConfig
+{
+    // Địa chỉ contract ERC721
+    public static readonly string NFTTokenGateContractAddress = "0xCA3eF9c4d636ba1A70273e7da95b0F9f34E42ed0";
+
+    // Địa chỉ contract ERC721 khác (nếu khác với TokenGate)
+    public static readonly string NFTVIPContractAddress = "0x0a48cFAF1e3E99408646b2036a9dfa9433545650";
+
+    // Địa chỉ contract ERC20 - GEM Token
+    public static readonly string TokenGemContractAddress = "0x0B0406a53bEe644E521622d68E84d2788051BF93";
+
+}
